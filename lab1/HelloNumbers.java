26e76ccabea6749beaa0207a39ed5c79a62c168b
Neel Dhoundiyal <neeldhoundiyal6@gmail.com>
2019-05-20 12:50:02 +0400

    function modified

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,9 +1,12 @@
 public class HelloNumbers {
     public static void main(String[] args) {
         int x = 0;
+        int output = 0 ;
         while (x < 10) {
-            System.out.print(x + " ");
+            System.out.print(output + " ");
             x = x + 1;
+            output = output + x;
+
         }
     }
 }
