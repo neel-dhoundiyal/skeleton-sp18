579ccd2efb81128241ae44acbf536f53e98c0142
Neel Dhoundiyal <neeldhoundiyal6@gmail.com>
2019-05-20 12:40:26 +0400

    function modified

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -8,10 +8,10 @@ public class LeapYear {
      */
     public static boolean isLeapYear(int year){
       if((year%400 ==0) or ((year%4==0)and(year%100!=0))){
-        return True;
+        return true;
       }
       else {
-        return False;
+        return false;
       }
 
     }
